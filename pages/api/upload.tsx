0da1e77a78a301920606ce9ccb405522e1d55b9f
tudor-manea <117779538+tudor-manea@users.noreply.github.com>
2024-05-16 22:10:01 +0100

    made some fixes

--- pages/api/upload.tsx
+++ pages/api/upload.tsx
@@ -1,50 +1,49 @@
-import {v2 as cloudinary} from "cloudinary"
-import prisma from "@/lib/prisma"
-import multer from "multer"
+import { v2 as cloudinary } from "cloudinary";
+import prisma from "@/lib/prisma";
+import multer from "multer";
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
 
 cloudinary.config({
   cloud_name: "dgm92vwvw",
-  api_key:"421987822545236",
-  api_secret:"FIs8MGqMUBxCuOYdPVz0F59KAxI"
+  api_key: "421987822545236",
+  api_secret: "FIs8MGqMUBxCuOYdPVz0F59KAxI",
 });
 
 const upload = multer({ dest: "uploads/" });
 
 export const config = {
   api: {
-    bodyParser: false
+    bodyParser: false,
   },
-}
+};
 
-export default async function handler
-(req, res) {
-    if (req.method === "POST") {
-    upload.single("image")(req, res, async (err) => {
-      if(err) {
-        console.error(err);
-        return res.status(500).json(
-          {error: "Image upload failed"});
-      }
+const handler = nextConnect<NextApiRequest, NextApiResponse>();
 
-      const { description } = req.body;
-      const { path } = req.file;
+handler.use(upload.single("image"));
 
-      try {
-        const result = await cloudinary.uploader.upload(path);
-        const image = await prisma!.art.create({
-          data: {
-            description,
-            imageUrl: result.secure_url,
-          },
-        });
+handler.post(async (req: NextApiRequest & { file: Express.Multer.File }, res: NextApiResponse) => {
+  const { description } = req.body;
+  const { path } = req.file;
 
-        return res.status(201).json(image);
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({error: "Image upload failed"});
-      }
+  try {
+    const result = await cloudinary.uploader.upload(path);
+    const image = await prisma!.art.create({
+      data: {
+        description,
+        imageUrl: result.secure_url,
+      },
     });
-  } else {
-    res.status(405).json({error: "Method not allowed" });
+
+    return res.status(201).json(image);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Image upload failed" });
   }
-}
+});
+
+handler.use((req: NextApiRequest, res: NextApiResponse) => {
+  res.status(405).json({ error: "Method not allowed" });
+});
+
+export default handler;
