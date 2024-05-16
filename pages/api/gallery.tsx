37128e8ca8fcb4f722bcaed5fcb399a0014c5a28
tudor-manea <117779538+tudor-manea@users.noreply.github.com>
2024-05-16 22:01:26 +0100

    made some fixes to api/gallery

--- pages/api/gallery.tsx
+++ pages/api/gallery.tsx
@@ -1,12 +1,12 @@
 import prisma from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const images = await prisma!.art.findMany()
     res.status(200).json(images)
   } catch (error) {
     console.log(error)
-    res.status(500).json({error: "Failed to fetch images"})
+    res.status(500).json({ error: "Failed to fetch images" })
   }
 }
-
